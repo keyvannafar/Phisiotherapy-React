54a17e82c0c6f275c3497367c83ce4f4cd4eddf4
unknown <kayvannafarzadeh@yahoo.com>
2022-12-11 16:04:43 +0330

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "animate.css";
+function App() {
+ <h1>Phisiotherapy Clinic</h1>
+}
+export default App;
