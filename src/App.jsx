d32fff259d8b0e1ff5efc3e00379df891a5e246e
unknown <kayvannafarzadeh@yahoo.com>
2022-12-14 02:27:00 +0330

    Footer and Header-Redux

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,19 @@ import "./App.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "animate.css";
+import store from "./redux/store";
 import Navbar from "./layout/Navbar/Navbar";
 import Footer from "./layout/Footer";
+import { Provider } from "react-redux";
 function App() {
   return (
     <>
-      <Navbar />
-      <Footer />
+      <BrowserRouter>
+        <Provider store={store}>
+          <Navbar />
+          <Footer />
+        </Provider>
+      </BrowserRouter>
     </>
   );
 }
