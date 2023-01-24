dfd18ffdc3457791eeb1e6bfbcc5750d50845268
unknown <kayvannafarzadeh@yahoo.com>
2023-01-24 19:42:04 +0330

    Bilingualization of one of the sections(soundof your body

--- src/ourQuality/index.jsx
+++ src/ourQuality/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "../Slider/Slider.css";
 import "./quality.css";
 import slide1 from "../images/portfolio/pardis-clinic-2.jpg";
 import slide2 from "../images/portfolio/pardis-clinic-3.jpg";
 import slide4 from "../images/portfolio/pardis-clinic-6.jpg";
 export default function Quality() {
+  const Language = useSelector((state) => state.LanChange.Language);
   return (
     <div className="d-flex w-100 justify-content-around qualityMain">
       <div className="w-50 d-flex justify-content-around pt-2 pb-2 position-relative">
@@ -83,25 +85,29 @@ export default function Quality() {
       <div className="w-50 pt-5 pb-5 d-flex justify-content-around">
         <div className="d-flex justify-content-center align-items-center">
           <div>
-            <h5 className="p-2"> !به صدای بدن خود توجه کنید</h5>
-            <br />
-            <h6 className="d-flex justify-content-center">
-              ...و با ما دردهایتان را مدیریت کنید
-            </h6>
-            <br />
-            {/* <span className="d-flex justify-content-center font-italic text-white">
-              همه روزه با تعیین وقت قبلی
-            </span>
-
-            <div className="d-flex justify-content-center border-1"> */}
-              {/* {" "}
-                  :راه های تماس <i class="bi bi-person-lines-fill"></i>
-                  <br /> */}
-              {/* <div className="d-flex justify-content-between"> */}
-              {/* <i class="bi bi-telephone-inbound text-white"></i> &nbsp;
-              <i class="bi bi-instagram text-white"></i> &nbsp;
-              <i class="bi bi-envelope text-white"></i> 
-            </div> */}
+            {Language == false ? (
+              <div>
+                <h5 className="p-2"> !به صدای بدن خود توجه کنید</h5>
+                <br />
+                <h6 className="d-flex justify-content-center">
+                  ...و با ما دردهایتان را مدیریت کنید
+                </h6>
+                <br />
+              </div>
+            ) : (
+              <div>
+                <h5 className="p-2">
+                  {" "}
+                  Pay attention to the sound of your 
+                  body!
+                </h5>
+                <br />
+                <h6 className="d-flex justify-content-center">
+                  And manage your pain with us...
+                </h6>
+                <br />
+              </div>
+            )}
           </div>
         </div>
       </div>
