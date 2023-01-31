223034e453e449a074da6f3c102b653cfa5b102d
Keyvan Nafarzadeh <kayvannafarzadeh@yahoo.com>
2023-01-31 18:55:13 +0330

    install React-Lazy-Load

--- src/Sectionthree/dataImages/index.js
+++ src/Sectionthree/dataImages/index.js
@@ -2,12 +2,14 @@
 
 import React, { useState } from "react";
 function ImageData({image}){
-   return(
-    <div >
-        
-        <img className="imagesPortfolio" src={image} />
-    </div>
-   )
+   return (
+     <div>
+         <img
+           className="imagesPortfolio animate__animated animate__flipInY"
+           src={image}
+         />
+     </div>
+   );
 }
 
 export default ImageData
--- src/Sectionthree/index.jsx
+++ src/Sectionthree/index.jsx
@@ -2,19 +2,20 @@ import "./Sectionthree.css";
 import dataImage from "./dataImages/data";
 import { useSelector } from "react-redux/es/exports";
 import ImageData from "./dataImages";
+import LazyLoad from "react-lazyload";
 function Sectionthree() {
   const Language = useSelector((state) => state.LanChange.Language);
   return (
     <div className="full-width" id="gallery">
+        <span className="text-black">salam</span>
       <h2 className="text-center about-header">
         {Language == false ? (
           <span>گالری تصاویر</span>
         ) : (
           <span>Photo Gallery</span>
         )}
-      </h2>
+      </h2>{" "}
       <div className="container d-flex sectionthree">
-        {/* <div className="threeleft"> */}
         <div className="row row-cols-1  row-cols-md-3 row-cols-lg-3 g-4 mt-2 ">
           {dataImage.map((item) => (
             <ImageData key={item.id} {...item} />
@@ -22,7 +23,6 @@ function Sectionthree() {
         </div>
       </div>
     </div>
-    //  </div>
   );
 }
 
