21d3407fe918b48a0837fa111f68db9eebfbf513
Keyvan Nafarzadeh <kayvannafarzadeh@yahoo.com>
2023-02-10 18:17:06 +0330

    Start creating LightBox

--- src/Sectionthree/dataImages/index.js
+++ src/Sectionthree/dataImages/index.js
@@ -1,21 +1,26 @@
-
-
 import React, { useState } from "react";
 import LazyLoad from "react-lazyload";
 function ImageData({ image, animTime }) {
+  const [zoom, setZoom] = useState(true);
+  const lightBox = () => {
+    zoom == true ? setZoom(false) : setZoom(true);
+  };
   return (
     <div className="d-flex galleryImage">
       <LazyLoad offset={-100}>
-        <div className="zoomImages d-flex animate__animated animate__zoomIn animate__delay-1s">
+        <div className="zoomImages d-flex animate__animated animate__zoomIn animate__delay-1s ">
           <i class="bi bi-zoom-in text-white"></i>
         </div>
         <img
-          className={`imagesPortfolio animate__animated animate__slideInUp justify-content-center ${animTime}`}
+          className={`${
+            zoom == true ? "imagesPortfolio" : "imagesPortfolioLightBox"
+          }  animate__animated animate__slideInUp justify-content-center ${animTime}`}
           src={image}
+          onClick={lightBox}
         />
       </LazyLoad>
     </div>
   );
 }
 
-export default ImageData
+export default ImageData;
